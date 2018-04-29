aafa6fe72e001f92ff15a3db32aede0f8d0a0bd1
Nikolas Yanez <yaneznikolas@gmail.com>
2018-04-29 14:59:17 -0400

    fixed styling for mobile and table events/added newslettercomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,20 @@ import { CollapseModule, BsDropdownModule } from 'ngx-bootstrap';
 // Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ChlogoComponent } from './chlogo/chlogo.component';
+import { EventsComponent } from './events/events.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
 
 // Font Awesome Icons
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ChlogoComponent } from './chlogo/chlogo.component';
-import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ChlogoComponent,
-    EventsComponent
+    EventsComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
